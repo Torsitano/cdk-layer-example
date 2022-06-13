fca6ad18425c2ae21d8cf7549a200a4ad072a6a4
Shawn Torsitano <shawn.torsitano@gmail.com>
2022-06-12 21:15:00 -0400

    cdk init

--- lib/main.ts
+++ lib/main.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkTemplateStack } from './stacks/cdkTemplateStack';
+import { CdkLayerExampleStack} from './stacks/cdkLayerExampleStack';
 
 const app = new cdk.App();
-new CdkTemplateStack(app, 'CdkTemplateStack', {
+new CdkLayerExampleStack(app, 'CdkLayerExampleStack', {
 
     
 });
--- lib/stacks/cdkLayerExampleStack.ts
+++ lib/stacks/cdkLayerExampleStack.ts
@@ -2,7 +2,7 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 
-export class CdkTemplateStack extends Stack {
+export class CdkLayerExampleStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
